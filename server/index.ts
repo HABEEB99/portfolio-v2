d4428dcc83ecfcf9a4dbaf16143ce308925e5c3b
Habeeb <habeebahmadu1@gmail.com>
2024-03-16 22:03:50 +0100

    feat: setup next.js with typescript

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-const PORT = 4000;
-
-app.get("/", (req, res) => res.send({ message: "HELLO FROM SERVER" }));
-
-app.listen(PORT, () => console.log(`app running on port ${PORT}`));
